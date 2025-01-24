00139f03baa604bfc48b2c179d81a12c967b0151
Brian Solano <brian_solano110012@yahoo.com>
2025-01-23 23:45:27 -0600

    added login method for authapi tsx

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,7 +2,24 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return response;
+  } catch (error) {
+    console.error("Error during login request:", error);
+  }
+};
 
 
 
